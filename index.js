f80117dfa91d294d2847d8fc43e7a12a2c4e4e0a
harsh <scotedflotsin.co.ltd@gmail.com>
2026-06-28 23:56:34 +0530

    class setup

---
+++ index.js
@@ -0,0 +1,7 @@
+import { PyayFusion } from "./packages/core/index.js";
+
+
+
+
+
+const tes = new PyayFusion("Hello");
---
+++ packages/core/index.js
@@ -0,0 +1,12 @@
+export class PyayFusion{
+    constructor(config){
+        try{
+//Etracting the paltform level config
+//Supported platforms {"Stripe", "Razorpay", "Lemon Squeezy", "PayPal", "Cashfree", "Paddle"}
+
+
+        }catch(e){
+throw e;
+        }
+    }
+}
